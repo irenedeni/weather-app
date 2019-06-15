00ca8cd9320d5dc89c017cd3314f34fc2b4307a2
ireneden <irene.denicolo89@gmail.com>
2019-06-15 16:34:46 +0200

    installed EJS and body-parser

--- server.js
+++ server.js
@@ -19,8 +19,11 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+
+app.post('/', function (req, res) {
+  res.render('index');
 })
 
 app.listen(3000, function () {
